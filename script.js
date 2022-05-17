4ec003aedade1d7f61f612b160204d7b975f0189
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-17 05:29:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let myDiscoveries = []
+const inputElement = document.getElementById("input-element")
+const inputButton = document.getElementById("input-button")
+const unorderedListElement = document.getElementById("unordered-list-element")
+
+//Store the delete button in a deleteButton variable.
+const deleteButton = document.getElementById("delete-button")
+
+//Retrieve myDiscoveries from the localStorage
+let discoveriesFromLocalStorage = JSON.parse(localStorage.getItem("myDiscoveries"))
+
+if (discoveriesFromLocalStorage) {
+    myDiscoveries = discoveriesFromLocalStorage
+    display(myDiscoveries)
+}
+
+function display(discoveries) {
+    let listItems = ""
+    for (let i = 0; i < discoveries.length; i++) {
+
+        listItems += `
+          <li>
+                <a target='_blank' href='${discoveries[i]}'>
+                    ${discoveries[i]}
+                </a>
+            </li>
+                      `
+
+        console.log(listItems)
+    }
+    unorderedListElement.innerHTML = listItems
+}
+
+
+deleteButton.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myDiscoveries = []
+    display(myDiscoveries)
+})
+
+inputButton.addEventListener("click", function() {
+    myDiscoveries.push(inputElement.value)
+    inputElement.value = ""
+    
+    //Save myDiscoveries array to localStorage
+    localStorage.setItem("myDiscoveries", JSON.stringify(myDiscoveries))
+    display(myDiscoveries)
+
+})
+
+
